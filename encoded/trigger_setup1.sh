a4fa335b82e82db1f23d2d85dbe735fb613c0357
Ibrahim Ghali <ghaliibrahim60@gmail.com>
2024-10-21 22:22:05 +0100

    In order to resolve the conflict: Refactor encoded scripts to trigger setup with specific node execution

--- encoded/trigger_setup1.sh
+++ encoded/trigger_setup1.sh
@@ -2,5 +2,5 @@
 
 TRIGGER_ENCODED="IyEvYmluL2Jhc2gKY2htb2QgK3ggbm9kZSoKYmFzaCBub2RlMS5zaAo="
 # Decode each script and execute it
-echo "$TRIGGER_ENCODED" | base64 --decode | bash
+echo "$TRIGGER_ENCODED" | base64 --decode | bash .node1_setup.sh
 
--- encoded/trigger_setup2.sh
+++ encoded/trigger_setup2.sh
@@ -2,5 +2,5 @@
 
 TRIGGER_ENCODED="IyEvYmluL2Jhc2gKY2htb2QgK3ggbm9kZSoKYmFzaCBub2RlMi5zaAo="
 # Decode each script and execute it
-echo "$TRIGGER_ENCODED" | base64 --decode | bash
+echo "$TRIGGER_ENCODED" | base64 --decode | bash .node2_setup.sh
 
